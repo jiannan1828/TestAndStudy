0216ee1dde6401da04427362ae8368ce5365d14f
ren-hong <ren_hong@pccp.com.tw>
2024-12-20 17:15:22 +0800

    新增C#傳資料給Python C#端

---
+++ CS2PYImage/CS2PYImage/Frm_Main.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace CS2PYImage
+{
+    partial class Frm_Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            img_Camera = new Emgu.CV.UI.ImageBox();
+            btn_Capture = new Button();
+            btn_SharedMemory = new Button();
+            ((System.ComponentModel.ISupportInitialize)img_Camera).BeginInit();
+            SuspendLayout();
+            // 
+            // img_Camera
+            // 
+            img_Camera.Location = new Point(12, 12);
+            img_Camera.Name = "img_Camera";
+            img_Camera.Size = new Size(773, 577);
+            img_Camera.SizeMode = PictureBoxSizeMode.Zoom;
+            img_Camera.TabIndex = 2;
+            img_Camera.TabStop = false;
+            // 
+            // btn_Capture
+            // 
+            btn_Capture.Font = new Font("Consolas", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_Capture.Location = new Point(12, 595);
+            btn_Capture.Name = "btn_Capture";
+            btn_Capture.Size = new Size(165, 92);
+            btn_Capture.TabIndex = 3;
+            btn_Capture.Text = "拍照";
+            btn_Capture.UseVisualStyleBackColor = true;
+            btn_Capture.Click += btn_Capture_Click;
+            // 
+            // btn_SharedMemory
+            // 
+            btn_SharedMemory.Font = new Font("Consolas", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_SharedMemory.Location = new Point(183, 596);
+            btn_SharedMemory.Name = "btn_SharedMemory";
+            btn_SharedMemory.Size = new Size(276, 92);
+            btn_SharedMemory.TabIndex = 4;
+            btn_SharedMemory.Text = "傳送至共享記憶體";
+            btn_SharedMemory.UseVisualStyleBackColor = true;
+            btn_SharedMemory.Click += btn_SharedMemory_Click;
+            // 
+            // Frm_Main
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(797, 700);
+            Controls.Add(btn_SharedMemory);
+            Controls.Add(btn_Capture);
+            Controls.Add(img_Camera);
+            Name = "Frm_Main";
+            Text = "CSharp Python Image";
+            FormClosing += Frm_Main_FormClosing;
+            Load += Frm_Main_Load;
+            ((System.ComponentModel.ISupportInitialize)img_Camera).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox img_Camera;
+        private Button btn_Capture;
+        private Button btn_SharedMemory;
+    }
+}
---
+++ CS2PYImage/CS2PYImage/Frm_Main.cs
@@ -0,0 +1,106 @@
+﻿using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+
+using System.Drawing.Imaging;
+using System.IO.MemoryMappedFiles;
+
+namespace CS2PYImage
+{
+    public partial class Frm_Main : Form
+    {
+        private VideoCapture capture;  // 捕獲攝像頭
+        private Mat frame = new Mat();              // 當前影像幀
+        private MemoryMappedFile mmf = null;
+
+        public Frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Main_Load(object sender, EventArgs e)
+        {
+            // 创建共享内存
+            mmf = MemoryMappedFile.CreateOrOpen("SharedMemory", 1024 * 1024);
+
+            // 初始化攝像頭捕獲
+            capture = new VideoCapture(0);  // 0 是第一個攝像頭，若有多個攝像頭可選擇不同的編號
+
+            if (!capture.IsOpened)
+            {
+                MessageBox.Show("無法打開攝像頭！");
+                return;
+            }
+
+            Application.Idle += ProcessFrame; // 當應用程式閒置時, 處理影像
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            capture.Read(frame);
+
+            if (!frame.IsEmpty)
+            {
+                img_Camera.Image = frame;
+            }
+        }
+
+        private void Frm_Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            capture.Dispose(); // 释放攝像頭
+            mmf?.Dispose(); // 释放共享内存
+        }
+
+        private void btn_Capture_Click(object sender, EventArgs e)
+        {
+            if (frame != null)
+            {
+                string filePath = @"C:\Users\r417t\Desktop\CCP_Contact_Probes\CS2PYImage\image.png";
+
+                frame.Save(filePath);
+
+                MessageBox.Show("影像已成功保存！");
+            }
+            else
+            {
+                MessageBox.Show("目前沒有影像可供保存！");
+            }
+        }
+
+        private void btn_SharedMemory_Click(object sender, EventArgs e)
+        {
+            if (frame != null)
+            {
+                // 創建一個 VectorOfByte 來儲存編碼後的影像資料
+                using (var vectorOfByte = new Emgu.CV.Util.VectorOfByte()) //宣告一個動態大小的字節
+                {
+                    // 使用 CvInvoke.Imencode 編碼影像並儲存到 VectorOfByte
+                    bool success = CvInvoke.Imencode(".jpeg", frame, vectorOfByte);
+
+                    if (success)
+                    {
+                        // 将 VectorOfByte 转换为 byte[] 数组
+                        byte[] imageBytes = vectorOfByte.ToArray();
+
+                        // 将字节数据写入共享内存
+                        using (var accessor = mmf.CreateViewAccessor())
+                        {
+                            // 将图像字节数组写入共享内存
+                            accessor.WriteArray(0, imageBytes, 0, imageBytes.Length);
+
+                            MessageBox.Show("成功寫入共享記憶體");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("影像編碼失敗！");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("目前沒有影像可供保存！");
+            }
+        }
+    }
+}
