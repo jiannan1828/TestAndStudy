5f47d933d8073ca630fb74e51b63f650083ad3ba
ren_hong <ren_hong@pccp.com.tw>
2025-02-20 16:35:08 +0800

    新增按鍵偵測的Javascript

--- MVC/MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
+++ MVC/MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f61eb995a9a675ad11ded863dee41c83a66fbac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a89b9f1105bdba0f229ceed18d3cfca23d9dc3e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
