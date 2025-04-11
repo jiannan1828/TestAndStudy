f0550943ca43d7828695257fd60ba70c04b70755
aezakmu0@gmail.com <aezakmu0@gmail.com>
2025-04-11 14:13:37 +0800

    add button for changing the form text

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -41,6 +41,7 @@
             this.STOP = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -149,11 +150,22 @@
             this.textBox3.TabIndex = 10;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1088, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 85);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(27F, 48F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1565, 701);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.STOP);
@@ -188,6 +200,7 @@
         private System.Windows.Forms.Button STOP;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -135,5 +135,10 @@ namespace WindowsFormsApp1
             
             
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Text = "123hahahaha";
+        }
     }
 }
